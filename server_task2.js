54fa7528dc73bba9ebdcab10b0d21c7693899ad5
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2025-01-12 09:25:24 -0500

    2

--- server_task2.js
+++ server_task2.js
@@ -2,20 +2,16 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-// Helper function to serve HTML files
 const serveFile = (filePath, res) => {
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("Server Error");
     } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
     }
   });
 };
 
-// Create the server
 const server = http.createServer((req, res) => {
   const url = req.url;
 
@@ -26,12 +22,10 @@ const server = http.createServer((req, res) => {
   } else if (url === "/contact") {
     serveFile(path.join(__dirname, "contact.html"), res);
   } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("404 Not Found");
   }
 });
 
-// Listen on port 5000
 const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`The NodeJS server on port ${PORT} is now running….`);
